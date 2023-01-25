54a4e9787e7e83d814accf9439d7fc9a7a8b78fa
Nataliia <nataliyamaxi@icloud.com>
2023-01-25 23:21:44 +0300

    delete submit

--- src/components/Form/Submit/Submit.jsx
+++ src/components/Form/Submit/Submit.jsx
@@ -4,19 +4,12 @@ import GetAgreement from "../../../api/getAgreement";
 
 const Submit = () => {
 
-    const [ file, setFile ] = useState();
 
-    useEffect(() => {
-        GetAgreement.GetFile().then(resp => {
-            setFile(resp);
-        });
-    }, []);
-    console.log(typeof file.file)
 
     return (
         <div className="form-submit">
             <input type='submit' name='submit' value='Отправить заявку'/>
-            <a href={ file } download className='agree'>Нажимая кнопку отправки, вы даете согласие на обработку своих персональных данных</a>
+            <a href="" download className='agree'>Нажимая кнопку отправки, вы даете согласие на обработку своих персональных данных</a>
         </div>
     );
 };
