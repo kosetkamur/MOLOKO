963ed82841201a39b9ddb47483c17d1118cb42a8
kosetkamur <71492732+kosetkamur@users.noreply.github.com>
2023-02-27 10:44:15 +0300

    Update Delivery.jsx

--- src/components/Collaboration/Delivery/Delivery.jsx
+++ src/components/Collaboration/Delivery/Delivery.jsx
@@ -8,7 +8,7 @@ const Delivery = () => {
     useEffect(() => {
         const fetch = async() => {
             try{
-                let url = 'http://zinchi5d.beget.tech/api/cooperation.terms?type=delivery';
+                let url = 'https://ooomlk.ru/api/cooperation.terms?type=delivery';
                 let getData = await axios.get(url);
                 setContent(getData.data.data)
                 return getData.data.data;
